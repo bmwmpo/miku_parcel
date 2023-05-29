6d55606c067864d11e8c6faae2101e40877a8543
bmwmpo <49045940+bmwmpo@users.noreply.github.com>
2023-05-28 22:14:35 -0400

    circle to path

--- dev/index.e5947a3d.js
+++ dev/index.e5947a3d.js
@@ -1,15 +1,15 @@
 var grpNo = 0;
 var string1 = "(    wa ! )";
 const func1 = (string)=>{
-    var temp = document.getElementById("space");
+    var temp = document.getElementById("space1");
     if (grpNo === 4) {
         if (string1.includes("(")) {
             var size = string1.length;
             string1 = string1.replace("(", "");
             string1 = string1.replace(")", "");
-            temp.innerHTML = temp.innerHTML + "<p class='g" + grpNo + "'>" + string1 + "</p>";
+            temp.innerHTML = temp.innerHTML + "<tspan class='g" + grpNo + "'>" + string1 + "</tspan>";
         }
-    } else temp.innerHTML = temp.innerHTML + "<p class='g" + grpNo + "'>" + string + "</p>";
+    } else temp.innerHTML = temp.innerHTML + "<tspan class='g" + grpNo + "'>" + string + "</tspan>";
     grpNo += 1;
     if (grpNo === 5) {
         var temp_opacity = document.getElementsByClassName("g1")[0];
